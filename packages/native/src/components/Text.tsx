a124ed731728f26cc7ad0221f96b59bd73863807
Krishna Gujjjar <krishnagujjjar@gmail.com>
2024-08-04 23:52:57 +0530

    feat(components): :sparkles: introduce Text component with customizable size and color

---
+++ packages/native/src/components/Text.tsx
@@ -0,0 +1,38 @@
+import { SIZE } from '@/configs/properties';
+import { useMemo } from 'react';
+import { Text as TextComponent } from 'react-native';
+
+interface TextProps {
+  value: string;
+  color: string;
+  size: 'sm' | 'md' | 'lg' | number;
+}
+
+const getTwoLetters = (string: string): string => {
+  if (!string || string.trim() === '') {
+    return '?';
+  }
+
+  const initials = string
+    .trim()
+    .split(/\W+/)
+    .reduce((acc, word) => acc + word.charAt(0), '');
+
+  return initials.slice(0, 2) || initials.charAt(0);
+};
+
+const Text = (props: TextProps): JSX.Element => {
+  const size = useMemo(
+    () => (typeof props.size === 'string' ? SIZE[props.size] : props.size) / 3,
+    [props.size]
+  );
+
+  return (
+    <TextComponent
+      style={{ fontSize: size, fontWeight: 'bold', textTransform: 'uppercase', color: props.color }}
+    >
+      {getTwoLetters(props.value)}
+    </TextComponent>
+  );
+};
+export default Text;
