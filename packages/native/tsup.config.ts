cfa27fbe5d438ed7e53ef85040c9b0a8403d8ac7
Krishna Gujjjar <krishnagujjjar@gmail.com>
2024-08-05 00:09:16 +0530

    build: :building_construction: add treeshake and some external deps in tsup config

--- packages/native/tsup.config.ts
+++ packages/native/tsup.config.ts
@@ -1,16 +1,18 @@
-import { defineConfig } from "tsup";
-import type { Options } from "tsup";
+// check-spell:ignore treeshake
+
+import { defineConfig } from 'tsup';
+import type { Options } from 'tsup';
 
 export default defineConfig((options: Options) => ({
-	entry: {
-		index: "src/index.ts",
-	},
-	banner: {
-		js: "'use client'",
-	},
-	clean: true,
-	format: ["cjs", "esm"],
-	external: ["react"],
-	dts: true,
-	...options,
+  entry: {
+    index: 'src/index.tsx',
+  },
+  splitting: true,
+  // minify:true,
+  clean: true,
+  treeshake: true,
+  format: ['cjs', 'esm'],
+  external: ['react', 'react-native', 'react-native-svg'],
+  dts: true,
+  ...options,
 }));
