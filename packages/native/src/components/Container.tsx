48c78c2d5bf970205c37966b8532806f08cf6ea0
Krishna Gujjjar <krishnagujjjar@gmail.com>
2024-08-07 21:04:39 +0530

    style(components): :rotating_light: Format the container component

--- packages/native/src/components/Container.tsx
+++ packages/native/src/components/Container.tsx
@@ -1,4 +1,5 @@
-import { type PropsWithChildren, useMemo } from 'react';
+import { useMemo } from 'react';
+import type { PropsWithChildren } from 'react';
 import { View } from 'react-native';
 import type { ColorValue } from 'react-native';
 
